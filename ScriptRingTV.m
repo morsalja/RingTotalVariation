b6dd8c168bcc36560a21231dde1372c68d1f4743
morteza salehjahromi <36670371+morsalja@users.noreply.github.com>
2019-02-25 11:18:49 -0500

    Update ScriptRingTV.m

--- ScriptRingTV.m
+++ ScriptRingTV.m
@@ -4,9 +4,9 @@ clear
 load('FBPNegative40.mat')
 
 x=FBPNegative40;
-lambdaIn =0.01; %weight of the TV term in the cost function (see eq(7) in the manuscript)
-alpIn = 2; % 3 and 4 is the best then 2 then 1,2 is the best, 1 is the best
-Center=[256,256];% CHANGEABLE CHANGEABLE CHANGEABLE CHANGEABLE CHANGEABLE CHANGEABLE
+lambdaIn =0.01; 
+alpIn = 2; 
+Center=[256,256];
 itterRTV_IN=30;   
 
 y= RingTotalVariation(x,lambdaIn,alpIn,Center,itterRTV_IN);
